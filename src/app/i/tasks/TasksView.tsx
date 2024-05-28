51fcc156a96bfbd8274f77f2a363802dcffc2a9c
MikhailPtichkin <mikhailptichkin1988@gmail.com>
2024-05-28 17:42:51 +0300

    add tasks kanban view

--- src/app/i/tasks/TasksView.tsx
+++ src/app/i/tasks/TasksView.tsx
@@ -2,10 +2,25 @@
 
 import { useState } from "react"
 import { ListView } from "./list-view/ListView"
+import { useLocalStorage } from "@/hooks/useLocalStorage"
+import Loader from "@/components/ui/Loader"
+import { SwitcherView } from "./SwitcherView"
+import { KanbanView } from "./kanban-view/KanbanView"
 
 export type TypeView = "list" | "kanban"
 
 export const TasksView = () => {
-  const [view, setView] = useState<TypeView>("list")
-  return <ListView />
+  const [type, setType, isLoading] = useLocalStorage<TypeView>({ key: 'view-type', defaultValue: "list" })
+
+  if (isLoading) return <Loader />
+
+
+  return <div>
+
+    <SwitcherView
+      setType={setType}
+      type={type}
+    />
+    {type === "list" ? <ListView /> : <KanbanView />}
+  </div>
 }
